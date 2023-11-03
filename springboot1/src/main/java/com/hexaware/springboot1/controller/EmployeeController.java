2566c0fd1c3b061554d2ff6f3fa611af6faef6ed
javeed512 <javeedmohammedjaveed@gmail.com>
2023-11-03 18:51:27 +0530

    Spring MVC updated project , assignment

--- springboot1/src/main/java/com/hexaware/springboot1/controller/EmployeeController.java
+++ springboot1/src/main/java/com/hexaware/springboot1/controller/EmployeeController.java
@@ -1,12 +1,9 @@
 package com.hexaware.springboot1.controller;
 
-import java.net.http.HttpRequest;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hexaware.springboot1.entity.Employee;
@@ -21,18 +18,51 @@ public class EmployeeController {
 		return "add_employee";
 	}
 
-	@RequestMapping(path = "/add", method = RequestMethod.POST)
-	@ResponseBody
-	public String addEmployeeData(HttpServletRequest request) {
-
-		int eid = Integer.parseInt(request.getParameter("eid")); // "101"
-		String ename = request.getParameter("ename");
-		double salary = Double.parseDouble(request.getParameter("salary"));
-
-		Employee emp = new Employee(eid, ename, salary);
-
-		return   "<h1 style ='color:green'>"+emp.toString()+"<h1>";
-
-	}
+	/*
+	 * @RequestMapping(path = "/add", method = RequestMethod.POST)
+	 * 
+	 * @ResponseBody public String addEmployeeData(HttpServletRequest request) {
+	 * 
+	 * int eid = Integer.parseInt(request.getParameter("eid")); // "101" String
+	 * ename = request.getParameter("ename"); double salary =
+	 * Double.parseDouble(request.getParameter("salary"));
+	 * 
+	 * Employee emp = new Employee(eid, ename, salary);
+	 * 
+	 * return "<h1 style ='color:green'>"+emp.toString()+"<h1>";
+	 * 
+	 * }
+	 */
+	
+		@RequestMapping(path = "/add", method = RequestMethod.POST)
+		@ResponseBody
+		public String addEmployee(@RequestParam int eid, @RequestParam String ename, @RequestParam double salary) {
+			
+			 Employee emp = new Employee(eid, ename, salary);
+			
+			 // service layer  --> dao layer ---> DB
+			 
+			 // service.add(emp)
+			 
+			
+			return emp.toString();
+			
+			
+		}
+	
+		@RequestMapping(path = "/getall", method = RequestMethod.GET)
+		@ResponseBody
+		public String  getAll() {
+			
+			
+			
+			
+		}	
+	
+	
+	
+	
+	
+	
 
 }
